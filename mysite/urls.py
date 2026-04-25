103f2c319327d4c35bb7f6582ce0408a8f6d58ee
Unnati310559 <unnati860@gmail.com>
2026-04-25 20:37:41 +0530

    fix media files for render deployment

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,12 +1,13 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# 🔴 TEMP FIX: media files serve on Render
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
